38dd0e19da7b9ca3d958531762a5488562fe4295
Samir Hamid <120100878+samir134b@users.noreply.github.com>
2023-01-17 14:49:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     render() {
-        const relativeList = ['Relative 1', 'Relative 2', 'Relative 3', 'Relative 4', 'Relative 5', 'Relative 6'];
-        return (
-          <div id="main">
-            <ol key="relativeList">
-              {relativeList.map((relative, idx) => (
-                <li key={`relativeListItem${idx+1}`}>{relative}</li>
-              ))}
-            </ol>
-          </div>
-        );
+
+        return(
+            <div id="main">
+               {/* Do not remove the main div */}
+               <ol key="relativeList">
+                <li key="relativeListItem1">Samir Hamid</li>
+                <li key="relativeListItem1">Mohammed Abaan</li>
+                <li key="relativeListItem1">Suhail</li>
+               </ol>
+            </div>
+        )
     }
 }
 
